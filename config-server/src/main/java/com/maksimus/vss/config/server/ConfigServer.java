a5d4a23bd84354b4b6626888db47302edbb46979
ogneyar79 <maksimus.s@mail.ru>
2023-03-27 11:11:03 +0300

    #4 Get date from config server

--- config-server/src/main/java/com/maksimus/vss/config/server/ConfigServer.java
+++ config-server/src/main/java/com/maksimus/vss/config/server/ConfigServer.java
@@ -1,7 +1,6 @@
 package com.maksimus.vss.config.server;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
